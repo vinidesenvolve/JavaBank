a21152464689873ab05ee52c2c38812490393443
Vinicius Alves Rodrigues <vinidesenvolve@gmail.com>
2021-04-12 12:17:27 -0300

    Testing method toPrintAccountData()

--- src/main/java/TestAccount.java
+++ src/main/java/TestAccount.java
@@ -9,15 +9,12 @@ public class TestAccount {
         account.balance=100;
         account.openingDate=java.time.LocalDate.now();
 
-        System.out.println(account.name);
-        System.out.println(account.openingDate);
-        System.out.println(account.number);
-
         account.printBalance(); //100
         account.toDeposit(100);
         account.printBalance(); //200
         account.toWithdraw(201);
         account.printBalance(); //150
         account.monthIncome(); //10%
+        account.toPrintAccountData();
     }
 }
